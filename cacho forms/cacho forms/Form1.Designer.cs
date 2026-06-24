a9bc4f2dc6af36a4b9231267efea0f8b83358007
Artur <abryanbg@gmail.com>
2026-06-24 18:36:57 -0400

    Interfaz terminada cacho

---
+++ cacho forms/cacho forms/Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace cacho_forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            chkDado1 = new CheckBox();
+            chkDado2 = new CheckBox();
+            chkDado3 = new CheckBox();
+            chkDado4 = new CheckBox();
+            chkDado5 = new CheckBox();
+            btnLanzar = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            btnBalas = new Button();
+            btnTontos = new Button();
+            btnTrenes = new Button();
+            btnEscalera = new Button();
+            btnFull = new Button();
+            btnPoker = new Button();
+            btnCuadras = new Button();
+            btnQuinas = new Button();
+            btnSenas = new Button();
+            btnGrande1 = new Button();
+            btnGrande2 = new Button();
+            lblMensaje = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // chkDado1
+            // 
+            chkDado1.AutoSize = true;
+            chkDado1.Location = new Point(30, 128);
+            chkDado1.Name = "chkDado1";
+            chkDado1.Size = new Size(68, 19);
+            chkDado1.TabIndex = 0;
+            chkDado1.Text = "Guardar";
+            chkDado1.UseVisualStyleBackColor = true;
+            // 
+            // chkDado2
+            // 
+            chkDado2.AutoSize = true;
+            chkDado2.Location = new Point(202, 128);
+            chkDado2.Name = "chkDado2";
+            chkDado2.Size = new Size(68, 19);
+            chkDado2.TabIndex = 1;
+            chkDado2.Text = "Guardar";
+            chkDado2.UseVisualStyleBackColor = true;
+            // 
+            // chkDado3
+            // 
+            chkDado3.AutoSize = true;
+            chkDado3.Location = new Point(364, 128);
+            chkDado3.Name = "chkDado3";
+            chkDado3.Size = new Size(68, 19);
+            chkDado3.TabIndex = 2;
+            chkDado3.Text = "Guardar";
+            chkDado3.UseVisualStyleBackColor = true;
+            chkDado3.CheckedChanged += chkdado3_CheckedChanged;
+            // 
+            // chkDado4
+            // 
+            chkDado4.AutoSize = true;
+            chkDado4.Location = new Point(518, 128);
+            chkDado4.Name = "chkDado4";
+            chkDado4.Size = new Size(68, 19);
+            chkDado4.TabIndex = 3;
+            chkDado4.Text = "Guardar";
+            chkDado4.UseVisualStyleBackColor = true;
+            // 
+            // chkDado5
+            // 
+            chkDado5.AutoSize = true;
+            chkDado5.Location = new Point(672, 128);
+            chkDado5.Name = "chkDado5";
+            chkDado5.Size = new Size(68, 19);
+            chkDado5.TabIndex = 4;
+            chkDado5.Text = "Guardar";
+            chkDado5.UseVisualStyleBackColor = true;
+            // 
+            // btnLanzar
+            // 
+            btnLanzar.Location = new Point(672, 169);
+            btnLanzar.Name = "btnLanzar";
+            btnLanzar.Size = new Size(75, 57);
+            btnLanzar.TabIndex = 5;
+            btnLanzar.Text = "Lanzar dados";
+            btnLanzar.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(30, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 101);
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(191, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 101);
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(655, 12);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(100, 101);
+            pictureBox3.TabIndex = 8;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(348, 12);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(100, 101);
+            pictureBox4.TabIndex = 9;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(501, 12);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(100, 101);
+            pictureBox5.TabIndex = 10;
+            pictureBox5.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.11258F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.44371F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.44371F));
+            tableLayoutPanel1.Controls.Add(btnBalas, 0, 0);
+            tableLayoutPanel1.Controls.Add(btnTontos, 0, 1);
+            tableLayoutPanel1.Controls.Add(btnTrenes, 0, 2);
+            tableLayoutPanel1.Controls.Add(btnEscalera, 1, 0);
+            tableLayoutPanel1.Controls.Add(btnFull, 1, 1);
+            tableLayoutPanel1.Controls.Add(btnPoker, 1, 2);
+            tableLayoutPanel1.Controls.Add(btnCuadras, 2, 0);
+            tableLayoutPanel1.Controls.Add(btnQuinas, 2, 1);
+            tableLayoutPanel1.Controls.Add(btnSenas, 2, 2);
+            tableLayoutPanel1.Controls.Add(btnGrande1, 0, 3);
+            tableLayoutPanel1.Controls.Add(btnGrande2, 2, 3);
+            tableLayoutPanel1.Controls.Add(lblMensaje, 1, 3);
+            tableLayoutPanel1.Location = new Point(30, 166);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Size = new Size(610, 220);
+            tableLayoutPanel1.TabIndex = 11;
+            tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
+            // 
+            // btnBalas
+            // 
+            btnBalas.Dock = DockStyle.Fill;
+            btnBalas.Location = new Point(3, 3);
+            btnBalas.Name = "btnBalas";
+            btnBalas.Size = new Size(195, 49);
+            btnBalas.TabIndex = 0;
+            btnBalas.Text = "Balas (-)";
+            btnBalas.UseVisualStyleBackColor = true;
+            // 
+            // btnTontos
+            // 
+            btnTontos.Dock = DockStyle.Fill;
+            btnTontos.Location = new Point(3, 58);
+            btnTontos.Name = "btnTontos";
+            btnTontos.Size = new Size(195, 49);
+            btnTontos.TabIndex = 1;
+            btnTontos.Text = "Tontos (-)";
+            btnTontos.UseVisualStyleBackColor = true;
+            // 
+            // btnTrenes
+            // 
+            btnTrenes.Dock = DockStyle.Fill;
+            btnTrenes.Location = new Point(3, 113);
+            btnTrenes.Name = "btnTrenes";
+            btnTrenes.Size = new Size(195, 49);
+            btnTrenes.TabIndex = 2;
+            btnTrenes.Text = "Trenes (-)";
+            btnTrenes.UseVisualStyleBackColor = true;
+            btnTrenes.Click += button4_Click;
+            // 
+            // btnEscalera
+            // 
+            btnEscalera.Dock = DockStyle.Fill;
+            btnEscalera.Location = new Point(204, 3);
+            btnEscalera.Name = "btnEscalera";
+            btnEscalera.Size = new Size(198, 49);
+            btnEscalera.TabIndex = 3;
+            btnEscalera.Text = "Escalera (-)";
+            btnEscalera.UseVisualStyleBackColor = true;
+            // 
+            // btnFull
+            // 
+            btnFull.Dock = DockStyle.Fill;
+            btnFull.Location = new Point(204, 58);
+            btnFull.Name = "btnFull";
+            btnFull.Size = new Size(198, 49);
+            btnFull.TabIndex = 4;
+            btnFull.Text = "Full (-)";
+            btnFull.UseVisualStyleBackColor = true;
+            // 
+            // btnPoker
+            // 
+            btnPoker.Dock = DockStyle.Fill;
+            btnPoker.Location = new Point(204, 113);
+            btnPoker.Name = "btnPoker";
+            btnPoker.Size = new Size(198, 49);
+            btnPoker.TabIndex = 5;
+            btnPoker.Text = "Poker (-)";
+            btnPoker.UseVisualStyleBackColor = true;
+            btnPoker.Click += button7_Click;
+            // 
+            // btnCuadras
+            // 
+            btnCuadras.Dock = DockStyle.Fill;
+            btnCuadras.Location = new Point(408, 3);
+            btnCuadras.Name = "btnCuadras";
+            btnCuadras.Size = new Size(199, 49);
+            btnCuadras.TabIndex = 6;
+            btnCuadras.Text = "Cuadras (-)";
+            btnCuadras.UseVisualStyleBackColor = true;
+            // 
+            // btnQuinas
+            // 
+            btnQuinas.Dock = DockStyle.Fill;
+            btnQuinas.Location = new Point(408, 58);
+            btnQuinas.Name = "btnQuinas";
+            btnQuinas.Size = new Size(199, 49);
+            btnQuinas.TabIndex = 7;
+            btnQuinas.Text = "Quinas (-)";
+            btnQuinas.UseVisualStyleBackColor = true;
+            // 
+            // btnSenas
+            // 
+            btnSenas.Dock = DockStyle.Fill;
+            btnSenas.Location = new Point(408, 113);
+            btnSenas.Name = "btnSenas";
+            btnSenas.Size = new Size(199, 49);
+            btnSenas.TabIndex = 8;
+            btnSenas.Text = "Senas (-)";
+            btnSenas.UseVisualStyleBackColor = true;
+            // 
+            // btnGrande1
+            // 
+            btnGrande1.Dock = DockStyle.Fill;
+            btnGrande1.Location = new Point(3, 168);
+            btnGrande1.Name = "btnGrande1";
+            btnGrande1.Size = new Size(195, 49);
+            btnGrande1.TabIndex = 9;
+            btnGrande1.Text = "Grande 1 (-)";
+            btnGrande1.UseVisualStyleBackColor = true;
+            btnGrande1.Click += button11_Click;
+            // 
+            // btnGrande2
+            // 
+            btnGrande2.Dock = DockStyle.Fill;
+            btnGrande2.Location = new Point(408, 168);
+            btnGrande2.Name = "btnGrande2";
+            btnGrande2.Size = new Size(199, 49);
+            btnGrande2.TabIndex = 11;
+            btnGrande2.Text = "Grande 2 (-)";
+            btnGrande2.UseVisualStyleBackColor = true;
+            // 
+            // lblMensaje
+            // 
+            lblMensaje.AutoSize = true;
+            lblMensaje.Dock = DockStyle.Fill;
+            lblMensaje.Location = new Point(204, 165);
+            lblMensaje.Name = "lblMensaje";
+            lblMensaje.Size = new Size(198, 55);
+            lblMensaje.TabIndex = 12;
+            lblMensaje.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnLanzar);
+            Controls.Add(chkDado5);
+            Controls.Add(chkDado4);
+            Controls.Add(chkDado3);
+            Controls.Add(chkDado2);
+            Controls.Add(chkDado1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox chkDado1;
+        private CheckBox chkDado2;
+        private CheckBox chkDado3;
+        private CheckBox chkDado4;
+        private CheckBox chkDado5;
+        private Button btnLanzar;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button btnBalas;
+        private Button btnTontos;
+        private Button btnTrenes;
+        private Button btnEscalera;
+        private Button btnFull;
+        private Button btnPoker;
+        private Button btnCuadras;
+        private Button btnQuinas;
+        private Button btnSenas;
+        private Button btnGrande1;
+        private Button btnGrande2;
+        private Label lblMensaje;
+    }
+}
---
+++ cacho forms/cacho forms/Form1.cs
@@ -0,0 +1,35 @@
+namespace cacho_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void chkdado3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
